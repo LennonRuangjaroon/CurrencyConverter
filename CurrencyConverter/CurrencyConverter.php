376ac9089bdff4832ffe7d28f84a1cdb95b3e012
Werawad Runagjaroon <lennon@Werawads-MacBook-Air.local>
2017-02-05 18:27:04 +0700

    modified:   CurrencyConverter/CurrencyConverter.php 	modified:   CurrencyConverterTest/CurrencyConverterTest.php

--- CurrencyConverter/CurrencyConverter.php
+++ CurrencyConverter/CurrencyConverter.php
@@ -1,14 +1,42 @@
 <?php
 
 /**
- * CurrencyConverter Test
+ * The CurrencyConverter
  * 
  * @author Lennon
  */
 class CurrencyConverter {
 
-	public function converter($amount, $currencyFrom, $currencyTo, $exchangeRate) {
-		return "400";
+	protected $exchangeRate = array('USD' => 40, "EUR" => 50);
+
+	/**
+	* This is Converter.
+	* Converter currency to THB
+	* @author Lennon
+	*/
+	public function converter($amount, $currencyTo) {
+		$this->vaildInput($amount, $currencyTo);
+
+		if(is_numeric($amount)) {
+			return ($amount * $this->getExchangeRate($currencyTo));
+		} else {
+			throw new InvalidArgumentException("Amount not numeric"); 
+		}
+	}
+
+	private function getExchangeRate($currencyTo) {
+		try{
+			return $this->exchangeRate[$currencyTo];
+		} catch (Exception $e) {
+			throw new InvalidArgumentException("CurrencyTo is not found");
+		}
+		
+	}
+
+	private function vaildInput($amount, $currencyTo){
+		if($amount == null || $currencyTo == null){
+			throw new InvalidArgumentException("Amount is not null and CurrencyTo is not null");
+		}
 	}
 
 }
