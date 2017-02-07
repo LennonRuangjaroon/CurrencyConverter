51fdd58fd7cef275b31d26c826e4ad5cb96abae2
Werawad Runagjaroon <lennon@Werawads-MacBook-Air.local>
2017-02-07 11:12:06 +0700

    modified CurrencyConverterTest.php

--- CurrencyConverterTest/CurrencyConverterTest.php
+++ CurrencyConverterTest/CurrencyConverterTest.php
@@ -1,13 +1,14 @@
 <?php
-require_once ('PHPUnit/Framework/TestCase.php');
+require_once ('../vendor/autoload.php');
 require_once ('../CurrencyConverter/CurrencyConverter.php');
 
 /**
  * CurrencyConverter Test
- * 
+ *
  * @author Lennon
  */
-class CurrencyConverterTest extends PHPUnit_Framework_TestCase {
+class CurrencyConverterTest extends PHPUnit\Framework\TestCase {
+
 
 	protected $currencyConverter;
 
@@ -21,21 +22,21 @@ class CurrencyConverterTest extends PHPUnit_Framework_TestCase {
 	/**
 	* @test
 	*/
-    public function should_be_can_conver_currency_10usd_to_thb() {
+    public function convert_currency_10usd_to_thb() {
         $this->assertCurrencyConverter(10, "USD", 400);
     }	
 
    	/**
 	* @test
 	*/
-    public function should_be_can_conver_currency_20usd_to_thb() {
+    public function convert_currency_20usd_to_thb() {
         $this->assertCurrencyConverter(20, "USD", 800);
     }	
 	
 	/**
 	* @test
 	*/
-    public function should_be_can_conver_currency_50usd_to_thb() {
+    public function convert_currency_50usd_to_thb() {
 		$this->assertCurrencyConverter(50, "USD", 2000);
     }
 
@@ -43,14 +44,14 @@ class CurrencyConverterTest extends PHPUnit_Framework_TestCase {
 	* @test
 	* @expectedException InvalidArgumentException
 	*/
-    public function should_be_exception_conver_currency_worng_amount_currency() {
+    public function convert_currency_worng_amount_currency_exception_throw() {
 		$this->currencyConverter->converter("worng", "USD");
     }
 
     /**
 	* @test
 	*/
-    public function should_be_can_conver_currency_50eur_to_thb() {
+    public function convert_currency_50eur_to_thb() {
 		$this->assertCurrencyConverter(10, "EUR", 500);
     }
 
@@ -58,15 +59,15 @@ class CurrencyConverterTest extends PHPUnit_Framework_TestCase {
 	* @test
 	* @expectedException InvalidArgumentException
 	*/
-    public function should_be_exception_conver_currency_worng_currency_type() {
-		$this->currencyConverter->converter(10, "worng");
+    public function convert_currency_with_wrong_currency_type_exception_throw(){
+    	$this->currencyConverter->converter(10, "worng");
     }
 
     /**
 	* @test
 	* @expectedException InvalidArgumentException
 	*/
-    public function should_be_exception_conver_currency_with_zero() {
+    public function covert_currency_with_zero_exception_throw(){
 		$this->currencyConverter->converter(0, 0);
     }
 
@@ -74,7 +75,7 @@ class CurrencyConverterTest extends PHPUnit_Framework_TestCase {
 	* @test
 	* @expectedException InvalidArgumentException
 	*/
-    public function should_be_exception_conver_currency_with_null_input() {
+    public function convert_currency_with_null_input() {
 		$this->currencyConverter->converter(null, null);
     }
 
